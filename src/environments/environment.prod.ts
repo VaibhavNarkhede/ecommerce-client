c1c1687f1557ab6bed88be10d57d4675cbea096f
vaibhav_msn <vaibhav_msn@outlook.com>
2019-09-01 22:12:41 +0530

    updated dist.zip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://3.84.7.41:4000'
+  baseUrl: 'http://10.0.2.15:4000' // Docker demo on Ubuntu VM
+  //baseUrl: 'http://3.84.7.41:4000'
 };
